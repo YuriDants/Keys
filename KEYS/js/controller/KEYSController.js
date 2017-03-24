86a0888393a3f18ed3c109b7697eb90783fc6fa7
Mateus de Lima <mateus.lima482@gmail.com>
2017-03-24 13:51:05 -0300

    Controller de adicionar adm

--- KEYS/js/controller/KEYSController.js
+++ KEYS/js/controller/KEYSController.js
@@ -1,4 +1,13 @@
 KEYSModulo.controller('KEYSController', function (KEYSService, $scope) {
+    $scope.adm = [];
+
+    $scope.adicionaAdm = function () {
+        $scope.adm.push({
+            id: $scope.adm.id,
+            nome: $scope.adm.nome,
+            email: $scope.aluno.email
+        });
+    };
 
  
 });
